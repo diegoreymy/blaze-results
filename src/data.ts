113ebda2311ac0913cd8d2b2d797a409e81ce2a1
Diego Humberto Reymy Aguilar <dreymy@avla.com>
2022-12-11 17:20:03 -0300

    add: volviendo a la version inicial del script

---
+++ src/data.ts
@@ -0,0 +1,9 @@
+export interface IBlazeRecentData {
+    id: String;
+    created_at: String;
+    color: Number;
+    roll: Number;
+    server_seed: String;
+}
+
+export const data: IBlazeRecentData[] = []
