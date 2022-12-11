fc3689a0c103ff334f97e275490801cc4392bd42
Diego Humberto Reymy Aguilar <dreymy@avla.com>
2022-12-11 15:25:29 -0300

    fix: resolviendo problema con url de la carpeta dist

--- src/app.ts
+++ src/app.ts
@@ -9,7 +9,7 @@ const wb = new xl.Workbook();
 const ws = wb.addWorksheet('Resultados Blaze');
 
 const app = express();
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3000;
 
 getResults();
 saveResultsAsXlsFile();
